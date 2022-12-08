7a582a6385c26ace1b0364992abbd178139cf83d
Jinsoo Choi <jinsoo2005@gmail.com>
2022-12-08 16:47:47 +0900

    ex4.4: created test function that sums the number of likes of all blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (posts) => {
+  let total = 0
+  posts.forEach(post => {
+    total += post.likes
+  })
+  return total
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
