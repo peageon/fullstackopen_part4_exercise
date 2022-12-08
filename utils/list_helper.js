72b586084e1dd6d0616dbe58dcc01381789e0ca9
Jinsoo Choi <jinsoo2005@gmail.com>
2022-12-08 17:04:13 +0900

    ex4.5: created helper function that finds the blog with the most likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,20 @@ const totalLikes = (posts) => {
   return total
 }
 
+const favouriteBlog = (posts) => {
+  let mostLiked
+  let likes = 0
+  posts.forEach(post => {
+    if (post.likes > likes) {
+      likes = post.likes
+      mostLiked = post
+    }
+  })
+  return (({ title,author,likes }) => ({ title,author,likes }))(mostLiked)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog
 }
